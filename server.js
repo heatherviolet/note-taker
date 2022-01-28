f294a623ab2d3fe0e80a259701e824a19b5c823f
Heather Violet <heather@heathers-air.lan>
2022-01-28 14:41:28 -0800

    add heroku

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+// HTML routes
+
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+app.get('/notes',  (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
