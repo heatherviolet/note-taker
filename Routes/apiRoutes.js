3438e04667f1c4eead05c1e12349aabc7309be1b
Heather Violet <heather@heathers-air.lan>
2022-01-29 23:17:57 -0800

    final commit

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -4,7 +4,7 @@ const { nanoid } = require('nanoid');
 const fs = require('fs');
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
+    res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 router.post('/notes', (req, res) => {
@@ -16,7 +16,7 @@ router.post('/notes', (req, res) => {
     newNote.id = nanoid(50);
     
     // writes new note to db.json
-    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(data), (err) => {
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), (err) => {
         if (err) throw err;
     });
         res.json(data);
