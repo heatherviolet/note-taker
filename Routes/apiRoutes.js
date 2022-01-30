c7fed0cf3cd1ba9132bdc60a712bab2044560ead
Heather Violet <heather@heathers-air.lan>
2022-01-29 22:04:04 -0800

    post updates

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,14 +1,25 @@
 const path = require('path');
 const router = require('express').Router();
+const { nanoid } = require('nanoid');
+const fs = require('fs');
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 router.post('/notes', (req, res) => {
-  //  retrieve all the notes
-  // add new note from req body to current notes
-  // return.res status to a 200-201
-})
+    const newNote = req.body;
+    // push new note
+    notes.push(newNote);
+    // creates unique id
+    newNote.id = nanoid(50);
+    
+    // writes new note to db.json
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(note), (err) => {
+        if (err) throw err;
+    });
+        res.json(newNote);
+    });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -14,16 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// receive a new note to save on the request body and add it do db.json, return new note
-app.post('api/notes', (req, res) => {
-    return res.sendStatus(200);
-});
+
 
 // deletes a note
 //app.delete('api/notes/:id') 
-app.use('/api', apiRoutes)
+app.use('/api', apiRoutes);
 
-app.use('/', htmlRoutes)
+app.use('/', htmlRoutes);
 
 // starts the server
 app.listen(PORT, () => {
