5c3d1caffc60157e234e412ab61cb7604bdfae9e
Heather Violet <heather@heathers-air.lan>
2022-01-29 22:45:49 -0800

    updates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Note Taker Starter Code
+# Note Taker
+This week I created a note taker app using express.js. The user is able to write the title and description for a note and then is able to save said notes on the page and also add additional notes. 
--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -4,22 +4,22 @@ const { nanoid } = require('nanoid');
 const fs = require('fs');
 
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
+    res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
 router.post('/notes', (req, res) => {
+    const data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));   
     const newNote = req.body;
     // push new note
-    notes.push(newNote);
+    data.push(newNote);
     // creates unique id
     newNote.id = nanoid(50);
     
     // writes new note to db.json
-    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(note), (err) => {
+    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(data), (err) => {
         if (err) throw err;
     });
-        res.json(newNote);
+        res.json(data);
     });
-
 
 module.exports = router;
